2861470f8b6c7b8f9e16b487a0d82437201d26b9
gabiga <bartoszgabiga@gmail.com>
2020-03-22 18:53:33 +0100

    Added files

---
+++ Script.js
@@ -0,0 +1,10 @@
+function changeClass(){
+    if (document.getElementById("star").classList.contains("glyphicon-star")){
+        document.getElementById("star").className ="glyphicon glyphicon-star-empty";
+    }
+    else {
+        document.getElementById("star").className ="glyphicon glyphicon-star";
+    }
+}
+    
+
